4af5ba9b7175eeb4cc8febade10d4eb054181cb0
DQuangggg <quangndhe150537@fpt.edu.vn>
2022-05-29 12:29:40 +0700

    Test 1
    Factory Method

---
+++ Test 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace Test1
+{
+    public interface Information
+    {
+        void Description();
+    }
+    public class Name : Information
+    {
+        public void Description() => WriteLine("My name is Nguyen Dang Quang");
+
+    }
+    public class StudenCode : Information
+    {
+        public void Description() => WriteLine("My student code is HE150537");
+
+    }
+
+    public abstract class Factory
+    {
+        public abstract Information CreateStudent();
+    }
+    public class NameFactory : Factory
+    {
+        public override Information CreateStudent() => new Name();
+    }
+
+    public class StudentCodeFactory : Factory
+    {
+        public override Information CreateStudent() => new StudenCode();
+
+    }
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("***Factory Method Pattern ***\n");
+            List<Factory> studnetFactorieList = new List<Factory>
+            {
+                new NameFactory(),
+                new StudentCodeFactory()
+            };
+
+            foreach (var student in studnetFactorieList)
+            {
+                student.CreateStudent().Description();
+            }
+            ReadLine();
+        }
+    }
+}
